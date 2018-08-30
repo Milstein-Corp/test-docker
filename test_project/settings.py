04e04c90b14a36373edd114ce0a290b75898a961
saadmk11 <saad.mk11@hotmail.com>
2018-08-30 19:15:18 +0600

    ENV variable added to Compose file

--- test_project/settings.py
+++ test_project/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': 'test_db',
+        'USER': 'test_user',
         'HOST': 'db',
         'PORT': 5432
     }
